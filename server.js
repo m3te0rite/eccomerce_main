87e1194252c4624cb82c4cc7e5d83053e46d8de1
Aarav <aarav.xtech@gmail.com>
2024-11-02 16:41:52 -0500

    finished server.js

--- server.js
+++ server.js
@@ -1,23 +1,3 @@
-
-import express from 'express';
-import dotenv from 'dotenv';
-// Load variables
-dotenv.config();
-// Start Server
-const app = express();
-app.use(express.static('public'));
-app.use(express.json());
-
-// home route
-
-app.get('/' )
-
-app.listen(3000, () =>{
-    console.log('listening on port 3000;')
-})
-
-/*
-
 import express from 'express';
 import dotenv from 'dotenv';
 import stripe from 'stripe';
@@ -86,4 +66,3 @@ app.listen(3000, () => {
     console.log('listening on port 3000;');
 });
 
-*/
