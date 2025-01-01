0b1a4b800ad9fd5b9f79cc7b577a576d808ead36
Aarav <aarav.xtech@gmail.com>
2025-01-01 11:12:25 -0600

    vercel settings

--- api/stripe-checkout.js
+++ api/stripe-checkout.js
@@ -1,30 +1,29 @@
-
+import express from 'express';
 import stripe from 'stripe';
 import dotenv from 'dotenv';
 
 dotenv.config();
 
-export default async function handler(req, res) {
-    if (req.method !== 'POST') {
-        return res.status(405).json({ message: 'Method not allowed' });
-    }
+const app = express();
+const stripeGateway = stripe(process.env.STRIPE_API);
+const DOMAIN = process.env.DOMAIN;
 
-    const stripeGateway = stripe(process.env.stripe_api);
-    const DOMAIN = process.env.DOMAIN;
+app.use(express.json());
 
+app.post("/stripe-checkout", async (req, res) => {
     const lineItems = req.body.items.map((item) => {
-        const unitAmount = parseInt(item.price.replace(/[^0-9.-]+/g, '') * 100);
-        return {
-            price_data: {
-                currency: 'usd',
-                product_data: {
-                    name: item.title,
-                    images: [item.productImg]
-                },
-                unit_amount: unitAmount,
-            },
-            quantity: item.quantity,
-        };
+       const unitAmount = parseInt(item.price.replace(/[^0-9.-]+/g, '') * 100);
+       return {
+        price_data: {
+        currency: 'usd',
+        product_data: {
+            name: item.title,
+            images: [item.productImg]
+        },
+        unit_amount: unitAmount,
+    },
+    quantity: item.quantity,
+       };
     });
 
     const session = await stripeGateway.checkout.sessions.create({
@@ -35,6 +34,7 @@ export default async function handler(req, res) {
         line_items: lineItems,
         billing_address_collection: "required",
     });
+    res.json({ url: session.url });
+});
 
-    res.json(session.url);
-}
+export default app;
